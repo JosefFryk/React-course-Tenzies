9f2303d1965736af4f081cd7a8668397a3e5865c
Josef Fryk <josef.fryk@gmail.com>
2022-05-13 16:35:38 +0200

    confetti

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from "react"
 import './App.css';
 import Die from "./Die"
 import {nanoid} from "nanoid"
+import Confetti from 'react-confetti'
+import {
+  useWindowSize,
+} from '@react-hook/window-size'
+
 
 
 
@@ -12,7 +17,7 @@ function App() {
  
   const [dice, setDice] = React.useState(allNewDice())
   const [tenzies, setTenzies] = React.useState(false)
-
+   
 
   function generateNewDie() {
 
@@ -48,8 +53,10 @@ const diceElements = dice.map(die => <Die key={die.id} value={die.value} isHeld=
   setDice(oldDice => oldDice.map(die => {
     
     if(tenzies === true) {
-      return generateNewDie()
-    
+       generateNewDie()
+       //force reload, kvuli tenzies false value, pozdeji zmenit
+       window.location.reload()
+       
     }
     else{
       return die.isHeld ? die : generateNewDie()
@@ -63,7 +70,7 @@ React.useEffect(() => {
   const allSameValue = dice.every(die => die.value === firstValue)
     if (allHeld && allSameValue) {
       setTenzies (true)
-      console.log("winner")
+     
     }else {
       setTenzies (false)
     }
@@ -71,25 +78,47 @@ React.useEffect(() => {
 
 
 
-
+const { width, height } = useWindowSize()
  return (
+ 
     <div className="wrapper">
       
     <main>
       {/* {tenzies &&} */}
       <h1 className="title">Tenzies</h1>
       
-      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      {tenzies ? <h2>You Won!!</h2> : <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>}
       
       <div className='dieBox'>
         {diceElements}
+      
       </div>
 
-     
+       {tenzies && <Confetti
+                 drawShape={ctx => {
+                  ctx.beginPath()
+                 for(let i = 0; i < 22; i++) {
+                    const angle = 0.35 * i
+                    const x = (0.2 + (1.5 * angle)) * Math.cos(angle)
+                    const y = (0.2 + (1.5 * angle)) * Math.sin(angle)
+                    ctx.lineTo(x, y)
+                 }
+                 ctx.stroke()
+                 ctx.closePath()
+                
+               }}
+               width= {width}
+               height = {height}
+               
 
+          />}
+        
+       <div>
       <button className="rollBtn" 
+     
       onClick={rollDice}>
         {tenzies ? "New Game" : "Roll"}</button>
+        </div>
 
 
     
